52077a53948b35a23de2a056b002632988e9b4ef
devsecops-user <mco.dimas@bankmandiri.co.id>
2021-10-24 15:00:56 +0700

    first init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IpServiceService } from './ip-service.service';  
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ipservice';
-}
+export class AppComponent implements OnInit {  
+  constructor(private ip:IpServiceService){}  
+  title = 'DemoApp';  
+  ipAddress:string;  
+ngOnInit()  
+  {  
+    this.getIP();  
+  }  
+  getIP()  
+  {  
+    this.ip.getIPAddress().subscribe((res:any)=>{  
+      this.ipAddress=res.ip;  
+    });  
+  }  
+} 
